36bbb2a59524320a8bd43787963467786a5b185f
stormyy00 <jonathan.trujillo0432@gmail.com>
2025-06-20 02:44:54 -0700

    init setup

---
+++ components/dashboard/dashboard.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Card from './card'
+import Toolbar from './toolbar'
+
+const Dashboard = () => {
+  return (
+    <div>
+      <Toolbar/>
+      <Card/>
+    </div>
+  )
+}
+
+export default Dashboard
