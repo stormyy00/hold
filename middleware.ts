e8d215db5aa85ad956df37666fa9c007ac06d77c
stormyy00 <jonathan.trujillo0432@gmail.com>
2025-06-28 16:35:26 -0700

    added middleware

--- middleware.ts
+++ middleware.ts
@@ -3,19 +3,19 @@ import { NextRequest, NextResponse } from "next/server";
 export const middleware = async (req: NextRequest) => {
   const res = NextResponse;
 
-  if (req.nextUrl.pathname.startsWith("/dashboard")) {
-    const session = req.cookies.get("session");
-    if (!session) {
-      return res.redirect(new URL("/", req.url));
-    }
-  }
+  //   if (req.nextUrl.pathname.startsWith("/dashboard")) {
+  //     const session = req.cookies.get("session");
+  //     if (session) {
+  //       return res.redirect(new URL("/", req.url));
+  //     }
+  //   }
 
-  const isSesionActive = req.cookies.get("session");
-  if (!isSesionActive) {
-    return res.redirect(new URL("/", req.url));
-  } else {
-    return NextResponse.redirect(new URL("/dashboard", req.url));
-  }
+  //   const isSesionActive = req.cookies.get("session");
+  //   if (isSesionActive) {
+  //     return res.redirect(new URL("/", req.url));
+  //   } else {
+  //     return NextResponse.redirect(new URL("/dashboard", req.url));
+  //   }
 };
 
 export const config = {
