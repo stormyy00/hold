66aecd1d8d18710b7b888b5d8c6b755d9e713d68
stormyy00 <jonathan.trujillo0432@gmail.com>
2025-06-29 01:17:13 -0700

    fixed middleware

--- middleware.ts
+++ middleware.ts
@@ -1,33 +1,25 @@
 import { NextRequest, NextResponse } from "next/server";
+import { getSessionCookie } from "better-auth/cookies";
+const protectedRoutes = ["/dashboard", "/folders"];
 
 export const middleware = async (req: NextRequest) => {
   const res = NextResponse;
+  const { pathname } = req.nextUrl;
+  const session = getSessionCookie(req);
 
-  //   if (req.nextUrl.pathname.startsWith("/dashboard")) {
-  //     const session = req.cookies.get("session");
-  //     if (session) {
-  //       return res.redirect(new URL("/", req.url));
-  //     }
-  //   }
+  if (pathname === "/" && session) {
+    return NextResponse.redirect(new URL("/dashboard", req.url));
+  }
 
-  //   const isSesionActive = req.cookies.get("session");
-  //   if (isSesionActive) {
-  //     return res.redirect(new URL("/", req.url));
-  //   } else {
-  //     return NextResponse.redirect(new URL("/dashboard", req.url));
-  //   }
+  const isProtected = protectedRoutes.some((route) =>
+    pathname.startsWith(route),
+  );
+  if (isProtected && !session) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+  return res.next();
 };
 
 export const config = {
-  matcher: [
-    "/dashboard/:path*",
-    "/folders/:path*",
-    "/links/:path*",
-    "/settings/:path*",
-    "/profile/:path*",
-    "/api/folders/:path*",
-    "/api/links/:path*",
-    "/api/settings/:path*",
-    "/api/profile/:path*",
-  ],
+  matcher: ["/dashboard/:path*", "/folders/:path*"],
 };
